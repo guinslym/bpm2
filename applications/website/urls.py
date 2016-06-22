91b251fca9ae4b47dbff1742c96aa84a9184c919
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-22 00:21:15 -0400

    adding modules

--- applications/website/urls.py
+++ applications/website/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     #url(r'^$', admin.site.urls),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
 ]
